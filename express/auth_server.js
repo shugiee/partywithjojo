a7a67ebf7ee2c6395f03e4900df24f9281099220
Jonathan Olson <jonathanholson@gmail.com>
2025-03-02 13:01:43 -0500

    Got super basic auth kind of working

--- express/auth_server.js
+++ express/auth_server.js
@@ -3,15 +3,17 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Enable form parsing
 app.use(cookieParser());
 
 const SECRET_PASSWORD = "1234"; // Change this to something better
 
 app.post("/login", (req, res) => {
+    console.log("express req.body", req.body);
     const { password } = req.body;
 
     if (password === SECRET_PASSWORD) {
-        res.cookie("rsvp_auth", "yes", {
+        res.cookie("rsvp_auth", "yes-foobar", {
             httpOnly: true,
             sameSite: "Strict", // Prevents CSRF
             maxAge: 24 * 60 * 60 * 1000, // 1 day
@@ -23,7 +25,7 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/rsvp", (req, res) => {
-    if (req.cookies.rsvp_auth === "yes") {
+    if (req.cookies.rsvp_auth === "yes-foobar") {
         res.send("Welcome to the RSVP page!");
     } else {
         res.status(403).send("Forbidden");
