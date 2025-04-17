c8e2ca8a5a5663739a99ca16a46e66b7958bed5f
Jonathan Olson <jonathanholson@gmail.com>
2025-04-17 15:18:56 -0400

    Continue on spotify; format details.ts file

--- public/details.js
+++ public/details.js
@@ -9,21 +9,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const getCookie = (name) => {
-    return Object.fromEntries(document.cookie
-        .split('; ')
-        .map(cookie => cookie.split('=')))[name] || null;
+    return (Object.fromEntries(document.cookie.split("; ").map((cookie) => cookie.split("=")))[name] || null);
 };
 // TODO only get relevant fields from Spotify
 const getPlaylistItems = () => __awaiter(void 0, void 0, void 0, function* () {
     const playlistId = "4Prc1zbrD2taosqI5usgcy";
-    const spotifyToken = getCookie("todoxcxc");
-    const playlistItemsResponse = yield fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=50&fields=items(track(name))`, {
+    const spotifyToken = getCookie("spotify");
+    const playlistItemsResponse = yield fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=50&fields=items(track(name,artists,album))`, {
         method: "GET",
         headers: {
-            "Authorization": `Bearer ${spotifyToken}`,
+            Authorization: `Bearer ${spotifyToken}`,
         },
     });
-    const items = yield playlistItemsResponse.json();
+    const items = (yield playlistItemsResponse.json());
     if (!playlistItemsResponse.ok) {
         throw new Error(`Response status: ${playlistItemsResponse.status}`);
     }
@@ -37,7 +35,7 @@ const addToPlaylist = (id) => __awaiter(void 0, void 0, void 0, function* () {
     const addToPlaylistResponse = yield fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${id}`, {
         method: "POST",
         headers: {
-            "Authorization": `Bearer ${spotifyToken}`,
+            Authorization: `Bearer ${spotifyToken}`,
             "Content-Type": "application/json",
         },
     });
@@ -49,29 +47,31 @@ const addToPlaylist = (id) => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 const playlistItem = ({ id, name, artists }) => `
-    <div class="playlist-item">
-        <div class="song-name">${name}</div>
-        <div class="artists">${artists.map(artist => artist.name).join(" ")}</div>
-    </div>
+<div class="playlist-item">
+<div class="spotify song-name">${name}</div>
+<div class="spotify artists">${artists.map((artist) => artist.name).join(" ")}</div>
+</div>
 `;
-const songSearchResult = ({ id, name, artists }) => `
-    <div class="search-result-container">
-        <button onclick="addToPlaylist('${id}')">
-            <div class="song-name">${name}</div>
-            <div class="artists">${artists.map(({ name }) => name).join(" ")}</div>
-        </button>
-    </div>
+const songSearchResult = (idx, { id, name, artists, album }) => `
+<button class="spotify-search-result">
+<span class="spotify song-index">${idx}</span>
+<img class="spotify-album-image" src="${album.images[0].url}"></img>
+<span class="spotify-song-title-and-artists">
+<div class="spotify spotify-song-title">${name}</div>
+<div class="spotify spotify-artists-names">${artists.map(({ name }) => name).join(" ")}</div>
+</span>
+</button>
 `;
-const searchResults = (songs) => `<div id="search-results">${songs.map(song => songSearchResult(song)).join("")}</div>`;
-const renderPlaylistItems = (songs) => `<div id="playlist-items">${songs.map(song => playlistItem(song)).join("")}</div>`;
+const searchResults = (songs) => `<div id="spotify-search-results">${songs.map((song, idx) => songSearchResult(idx, song)).join("")}</div>`;
+const renderPlaylistItems = (songs) => {
+    console.log("songs", songs);
+    return `<div id="playlist-items">${songs.map((song) => playlistItem(song)).join("")}</div>`;
+};
 const renderPlaylist = () => __awaiter(void 0, void 0, void 0, function* () {
     const playlistItems = getPlaylistItems();
     const playlistContainer = document.getElementById("spotify-playlist-items");
     const { items } = yield playlistItems;
-    const songs = items.map(({ track }) => {
-        const { name, artists, id } = track;
-        return { name, artists, id };
-    });
+    const songs = items.map((item) => item.track);
     if (playlistContainer) {
         playlistContainer.innerHTML = renderPlaylistItems(songs);
     }
@@ -82,24 +82,24 @@ const renderPlaylist = () => __awaiter(void 0, void 0, void 0, function* () {
 renderPlaylist();
 const searchSpotify = () => __awaiter(void 0, void 0, void 0, function* () {
     const spotifyToken = getCookie("spotify");
-    const text = document.getElementById("spotify-search").value;
+    const text = document.getElementById("spotify-search")
+        .value;
     const params = new URLSearchParams();
     params.append("q", text);
     params.append("type", "track");
     const spotifyResponse = yield fetch(`https://api.spotify.com/v1/search?${params}`, {
         method: "GET",
         headers: {
-            "Authorization": `Bearer ${spotifyToken}`
+            Authorization: `Bearer ${spotifyToken}`,
         },
     });
     if (!spotifyResponse.ok) {
         throw new Error(`Response status: ${spotifyResponse.status}`);
     }
-    const json = yield spotifyResponse.json();
-    const searchResultsElement = document.getElementById("spotify-search-results");
-    const songs = json.tracks.items.map(track => {
-        const { name, artists, id } = track;
-        return { name, artists, id };
+    const json = (yield spotifyResponse.json());
+    const searchResultsElement = document.getElementById("spotify-search-results-container");
+    const songs = json.tracks.items.map((track) => {
+        return track;
     });
     if (searchResultsElement) {
         searchResultsElement.innerHTML = searchResults(songs);
--- src/details.ts
+++ src/details.ts
@@ -1,141 +1,162 @@
 interface Artist {
-    name: string;
+  name: string;
+}
+
+interface AlbumImage {
+  height: number;
+  width: number;
+  url: string;
 }
 
 interface Song {
-    id: string;
-    name: string;
-    artists: Artist[];
+  id: string;
+  name: string;
+  artists: Artist[];
+  album: {
+    images: AlbumImage[];
+  };
 }
 
 interface SearchResults {
-    tracks: {
-        items: Song[];
-    }
+  tracks: {
+    items: Song[];
+  };
 }
 
 interface PlaylistItems {
-    items: { track: Song }[];
+  items: { track: Song }[];
 }
 
-
 const getCookie = (name: string) => {
-    return Object.fromEntries(
-        document.cookie
-        .split('; ')
-        .map(cookie => cookie.split('='))
-    )[name] || null;
+  return (
+    Object.fromEntries(
+      document.cookie.split("; ").map((cookie) => cookie.split("=")),
+    )[name] || null
+  );
 };
 
 // TODO only get relevant fields from Spotify
 const getPlaylistItems = async () => {
-    const playlistId = "4Prc1zbrD2taosqI5usgcy";
-    const spotifyToken = getCookie("todoxcxc");
-    const playlistItemsResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=50&fields=items(track(name))`, {
-        method: "GET",
-    headers: {
-        "Authorization": `Bearer ${spotifyToken}`,
+  const playlistId = "4Prc1zbrD2taosqI5usgcy";
+  const spotifyToken = getCookie("spotify");
+  const playlistItemsResponse = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=50&fields=items(track(name,artists,album))`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${spotifyToken}`,
+      },
     },
-    });
+  );
 
-    const items = await playlistItemsResponse.json() as PlaylistItems;
+  const items = (await playlistItemsResponse.json()) as PlaylistItems;
 
-    if (!playlistItemsResponse.ok) {
-        throw new Error(`Response status: ${playlistItemsResponse.status}`);
-    } else {
-        return items;
-    }
+  if (!playlistItemsResponse.ok) {
+    throw new Error(`Response status: ${playlistItemsResponse.status}`);
+  } else {
+    return items;
+  }
 };
 
 const addToPlaylist = async (id: string) => {
-    const playlistId = "4Prc1zbrD2taosqI5usgcy";
-    const spotifyToken = getCookie("spotify");
-    const addToPlaylistResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${id}`, {
-        method: "POST",
-    headers: {
-        "Authorization": `Bearer ${spotifyToken}`,
+  const playlistId = "4Prc1zbrD2taosqI5usgcy";
+  const spotifyToken = getCookie("spotify");
+  const addToPlaylistResponse = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${id}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${spotifyToken}`,
         "Content-Type": "application/json",
+      },
     },
-    });
+  );
 
-    if (!addToPlaylistResponse.ok) {
-        throw new Error(`Response status: ${addToPlaylistResponse.status}`);
-    } else {
-        console.log(`Added id ${id} to playlist!`);
-    }
+  if (!addToPlaylistResponse.ok) {
+    throw new Error(`Response status: ${addToPlaylistResponse.status}`);
+  } else {
+    console.log(`Added id ${id} to playlist!`);
+  }
 };
 
 const playlistItem = ({ id, name, artists }: Song) => `
-    <div class="playlist-item">
-        <div class="song-name">${name}</div>
-        <div class="artists">${artists.map(artist => artist.name).join(" ")}</div>
-    </div>
+<div class="playlist-item">
+<div class="spotify song-name">${name}</div>
+<div class="spotify artists">${artists.map((artist) => artist.name).join(" ")}</div>
+</div>
 `;
 
-const songSearchResult = ({ id, name, artists }: Song) => `
-    <div class="search-result-container">
-        <button onclick="addToPlaylist('${id}')">
-            <div class="song-name">${name}</div>
-            <div class="artists">${artists.map(({ name }) => name).join(" ")}</div>
-        </button>
-    </div>
+const songSearchResult = (idx: number, { id, name, artists, album }: Song) => `
+<button class="spotify-search-result">
+<span class="spotify song-index">${idx}</span>
+<img class="spotify-album-image" src="${album.images[0].url}"></img>
+<span class="spotify-song-title-and-artists">
+<div class="spotify spotify-song-title">${name}</div>
+<div class="spotify spotify-artists-names">${artists.map(({ name }) => name).join(" ")}</div>
+</span>
+</button>
 `;
 
-const searchResults = (songs: Song[]) => `<div id="search-results">${songs.map(song => songSearchResult(song)).join("")}</div>`;
-const renderPlaylistItems = (songs: Song[]) => `<div id="playlist-items">${songs.map(song => playlistItem(song)).join("")}</div>`;
+const searchResults = (songs: Song[]) =>
+  `<div id="spotify-search-results">${songs.map((song, idx) => songSearchResult(idx, song)).join("")}</div>`;
+const renderPlaylistItems = (songs: Song[]) => {
+  console.log("songs", songs);
+  return `<div id="playlist-items">${songs.map((song) => playlistItem(song)).join("")}</div>`;
+};
 
 const renderPlaylist = async () => {
-    const playlistItems = getPlaylistItems();
-    const playlistContainer = document.getElementById("spotify-playlist-items");
-    const { items } = await playlistItems;
-    const songs = items.map(({ track }) => {
-        const { name, artists, id } = track;
-        return { name, artists, id };
-    });
+  const playlistItems = getPlaylistItems();
+  const playlistContainer = document.getElementById("spotify-playlist-items");
+  const { items } = await playlistItems;
+  const songs = items.map((item) => item.track);
 
-    if (playlistContainer) {
-        playlistContainer.innerHTML = renderPlaylistItems(songs);
-    } else {
-        throw new Error("Could not find playlist container");
-    }
+  if (playlistContainer) {
+    playlistContainer.innerHTML = renderPlaylistItems(songs);
+  } else {
+    throw new Error("Could not find playlist container");
+  }
 };
 renderPlaylist();
 
 const searchSpotify = async () => {
-    const spotifyToken = getCookie("spotify");
-    const text = (<HTMLInputElement>document.getElementById("spotify-search")).value;
-    const params = new URLSearchParams();
-    params.append("q", text);
-    params.append("type", "track");
-    const spotifyResponse = await fetch(`https://api.spotify.com/v1/search?${params}`, {
-        method: "GET",
-    headers: {
-        "Authorization": `Bearer ${spotifyToken}`
+  const spotifyToken = getCookie("spotify");
+  const text = (<HTMLInputElement>document.getElementById("spotify-search"))
+    .value;
+  const params = new URLSearchParams();
+  params.append("q", text);
+  params.append("type", "track");
+  const spotifyResponse = await fetch(
+    `https://api.spotify.com/v1/search?${params}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${spotifyToken}`,
+      },
     },
-    });
+  );
 
-    if (!spotifyResponse.ok) {
-        throw new Error(`Response status: ${spotifyResponse.status}`);
-    }
+  if (!spotifyResponse.ok) {
+    throw new Error(`Response status: ${spotifyResponse.status}`);
+  }
 
-    const json = await spotifyResponse.json() as SearchResults;
-    const searchResultsElement = document.getElementById("spotify-search-results");
-    const songs = json.tracks.items.map(track => {
-        const { name, artists, id } = track;
-        return {name, artists, id};
-    });
-    if (searchResultsElement) {
+  const json = (await spotifyResponse.json()) as SearchResults;
+  const searchResultsElement = document.getElementById(
+    "spotify-search-results-container",
+  );
+  const songs = json.tracks.items.map((track) => {
+    return track;
+  });
 
+  if (searchResultsElement) {
     searchResultsElement.innerHTML = searchResults(songs);
-    } else {
-        throw new Error("Could not find search results element");
-    }
+  } else {
+    throw new Error("Could not find search results element");
+  }
 };
 
 const searchSubmitButton = document.getElementById("spotify-search-submit");
 if (searchSubmitButton) {
-    searchSubmitButton.addEventListener("click", searchSpotify);
+  searchSubmitButton.addEventListener("click", searchSpotify);
 } else {
-    throw new Error("Could not find search submit button");
+  throw new Error("Could not find search submit button");
 }
-
