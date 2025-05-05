50fff387cdad28d103f5c6acf957cb82cb24c42f
Jonathan Olson <jonathanholson@gmail.com>
2025-05-04 21:04:52 -0400

    Fixed google sheets, looks like I did some HTMl updates too

--- google-sheets/google-sheets.js
+++ google-sheets/google-sheets.js
@@ -1,14 +1,15 @@
-import fs from 'fs';
+import fs from "fs";
 import { get } from "http";
 import { config } from "dotenv";
-import { authenticate } from '@google-cloud/local-auth';
-import { google } from 'googleapis';
+import { authenticate } from "@google-cloud/local-auth";
+
+import { google } from "googleapis";
 
 const { readFile, writeFile } = fs.promises;
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-const TOKEN_PATH = '/etc/partywithjojo/token.json';
-const CREDENTIALS_PATH = '/etc/partywithjojo/credentials.json';
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+const TOKEN_PATH = "/etc/partywithjojo/token.json";
+const CREDENTIALS_PATH = "/etc/partywithjojo/credentials.json";
 
 const secretsPath = "/etc/partywithjojo/secrets.env";
 if (fs.existsSync(secretsPath)) {
@@ -18,108 +19,65 @@ if (fs.existsSync(secretsPath)) {
 }
 const { GOOGLE_SPREADSHEET_ID } = process.env;
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
-}
+const sheets = google.sheets("v4");
+const auth = new google.auth.GoogleAuth({
+  keyFile: "/etc/partywithjojo/google_sheets.json",
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
 
-/**
- * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await writeFile(TOKEN_PATH, payload);
-}
-
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
-
-
+const getRsvps = async () => {
+  get(
+    {
+      hostname: "localhost",
+      port: 3000,
+      path: "/rsvps",
+      agent: false,
+    },
+    (res) => {
+      const bodyChunks = [];
+      res
+        .on("data", function (chunk) {
+          bodyChunks.push(chunk);
+        })
+        .on("end", function () {
+          const rsvps = JSON.parse(Buffer.concat(bodyChunks));
+          writeRSVPs(rsvps).catch(console.error);
+        });
+    },
+  );
+};
 
 /**
  * Writes to "RSVPs DO NOT EDIT"
  * Docs: https://developers.google.com/sheets/api/samples/writing
  */
-async function writeRSVPs(auth, rsvps) {
-  const sheets = google.sheets({version: 'v4', auth});
-  const res = await sheets.spreadsheets.values.update({
+async function writeRSVPs(rsvps) {
+  const authClient = await auth.getClient();
+  const res = sheets.spreadsheets.values.update({
+    auth: authClient,
     spreadsheetId: GOOGLE_SPREADSHEET_ID,
     valueInputOption: "RAW",
     range: `RSVPs DO NOT EDIT!A3:E${3 + rsvps.length}`,
     requestBody: {
-        majorDimension: "ROWS",
-        range: `RSVPs DO NOT EDIT!A3:E${3 + rsvps.length}`,
-        values: rsvps.map(rsvp => { 
-            const welcomePartyInt = parseInt(rsvp.is_coming_to_welcome_party);
-            const weddingInt = parseInt(rsvp.is_coming_to_wedding);
-            const welcomePartyValue = isNaN(welcomePartyInt) ? "No response yet" : welcomePartyInt === 1 ? "Yes" : "No";
-            const weddingValue = isNaN(weddingInt)? "No response yet" : weddingInt === 1 ? "Yes" : "No";
-            return [
-                rsvp.name,
-                welcomePartyValue,
-                weddingValue
-            ]
-        }
-        )
-    }
+      majorDimension: "ROWS",
+      range: `RSVPs DO NOT EDIT!A3:E${3 + rsvps.length}`,
+      values: rsvps.map((rsvp) => {
+        const welcomePartyInt = parseInt(rsvp.is_coming_to_welcome_party);
+        const weddingInt = parseInt(rsvp.is_coming_to_wedding);
+        const welcomePartyValue = isNaN(welcomePartyInt)
+          ? "No response yet"
+          : welcomePartyInt === 1
+            ? "Yes"
+            : "No";
+        const weddingValue = isNaN(weddingInt)
+          ? "No response yet"
+          : weddingInt === 1
+            ? "Yes"
+            : "No";
+        return [rsvp.name, welcomePartyValue, weddingValue];
+      }),
+    },
   });
-    console.log("Response from google", res);
-};
+}
 
-const getRsvps = (auth) => {
-    get({
-        hostname: 'localhost',
-        port: 3000,
-        path: '/rsvps',
-        agent: false,  // Create a new agent just for this one request
-    }, (res) => {
-        // Buffer the body entirely for processing as a whole.
-        const bodyChunks = [];
-        res.on('data', function(chunk) {
-            // You can process streamed parts here...
-            bodyChunks.push(chunk);
-        }).on('end', function() {
-            const rsvps = JSON.parse(Buffer.concat(bodyChunks));
-            writeRSVPs(auth, rsvps).catch(console.error);
-        })
-    }); 
-};
-
-authorize().then(getRsvps).catch(console.error);
+getRsvps();
